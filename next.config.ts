5b50ad1f43a34c6176af4585aeeeb68a5dfd1f21
M. Fazri Nizar <anon6372098.id@gmail.com>
2025-11-28 23:59:57 +0700

    build: optimize firebase storage images

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "firebasestorage.googleapis.com",
+        pathname: "/v0/b/**",
+      },
+    ],
+    minimumCacheTTL: 2592000,
+  },
 };
 
 export default nextConfig;
